e99dff882fad79d19a49c25a3debe8ecd91f4399
Matthew Robillard <robillard.matthew22@gmail.com>
2025-06-09 21:55:46 -0700

    styling fixc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,8 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
+      <body className="antialiased">
         {children}
       </body>
     </html>
